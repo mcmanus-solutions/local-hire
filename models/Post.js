23ee0841d796b7a2b2d3a550b55400a18b1af81a
Guy McManus <guy@guymcmanus.com>
2019-08-17 15:34:34 +1000

    added in the post model

---
+++ models/Post.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
+
+// we are creating this model, because we want to keep the post even if the user decides to delete their account.  That is also why we are putting the name and the avatar here, so that we don't have to dig into the post
+
+// we also have a system for people to like and unlike.
+
+// I should add a rating and review and q and a thing similar to the comments area above in terms of the data structure
