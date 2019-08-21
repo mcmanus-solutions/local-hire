8bc5369aea342af08decc05cba9caf7cd05b046a
Guy McManus <guy@guymcmanus.com>
2019-08-21 19:53:47 +1000

    added in a mapping through of the profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,9 +7,16 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-      <Link to='/dashboard'>
-        <i className='fas fa-user' /> <span className='hide-sm'>Dashboard</span>
-      </Link>
+      <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
       <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt' />{' '}
@@ -22,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <a href='#!'>Developers</a>
+        <Link to='/profiles'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
