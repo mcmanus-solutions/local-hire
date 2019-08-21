5bd5b34b9a153be2450568a9e769bc177c3da4b1
Guy McManus <guy@guymcmanus.com>
2019-08-22 01:01:44 +1000

    added ability to see github repos in profile, along with number ofstars, watchers and forks

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -13,7 +13,6 @@ import {
 import setAuthToken from '../utils/setAuthToken';
 
 // Load User
-
 export const loadUser = () => async dispatch => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
