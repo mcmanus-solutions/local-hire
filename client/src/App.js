f2a9fde4203d9d680c579f49923fc3f5ca2f88cd
Guy McManus <guy@guymcmanus.com>
2019-08-22 13:15:50 +1000

    added single post display

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Profiles from './components/profiles/Profiles';
 import Dashboard from './components/dashboard/Dashboard';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
@@ -67,6 +68,7 @@ const App = () => {
                 component={AddEducation}
               />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </>
