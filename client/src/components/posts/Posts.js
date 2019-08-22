386b72cda1ca5c13761935e521c95878f2d8b7e8
Guy McManus <guy@guymcmanus.com>
2019-08-22 10:55:42 +1000

    added in the post item component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,13 +2,31 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
-import Spinner from '../../actions/post';
+import Spinner from '../../components/layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div />;
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user' />
+        Welcome ot the community
+      </p>
+      {/* {posts} */}
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
